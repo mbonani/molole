5b9cf89745798e8bf1e35bed78d1ae16a5206165
Stéphane Magnenat <stephanemagnenat@users.noreply.github.com>
2007-10-05 12:58:03 +0000

    Added missing I2C files

---
+++ i2c/i2c.h
@@ -0,0 +1,52 @@
+/*
+	Molole - Mobots Low Level library
+	An open source toolkit for robot programming using DsPICs
+	Copyright (C) 2007 Stephane Magnenat <stephane at magnenat dot net>
+	
+	Mobots group http://mobots.epfl.ch
+	Robotics system laboratory http://lsro.epfl.ch
+	EPFL Ecole polytechnique federale de Lausanne: http://www.epfl.ch
+	
+	See AUTHORS for more details about other contributors.
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _MOLOLE_I2C_H
+#define _MOLOLE_I2C_H
+
+/** I2C callback for status change */
+typedef void(*i2c_status_callback)();
+
+/** I2C callback when a data is available */
+typedef void(*i2c_set_data_callback)(unsigned char data);
+
+/** I2C callback when a data must be returned */
+typedef unsigned char(*i2c_get_data_callback)();
+
+// Functions, doc in the .c
+
+void i2c_init(unsigned char address);
+
+void i2c_init_slave(
+	i2c_status_callback message_from_master_callback,
+	i2c_status_callback message_to_master_callback,
+	i2c_set_data_callback data_from_master_callback,
+	i2c_get_data_callback data_to_master_callback,
+	int priority
+);
+
+void i2c_init_master(int priority);
+
+#endif
