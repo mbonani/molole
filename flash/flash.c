e16d29d01d14bd8ef66d1f530bac8958ef7531fb
Philippe Rétornaz <philippe.retornaz@epfl.ch>
2008-06-18 17:20:40 +0000

    Add flash page write & read module

---
+++ flash/flash.c
@@ -0,0 +1,221 @@
+/*
+	Molole - Mobots Low Level library
+	An open source toolkit for robot programming using DsPICs
+	
+	Copyright (C) 2008 Philippe Rétornaz <philippe.retornaz at epfl dot ch>,
+	Mobots group (http://mobots.epfl.ch), Robotics system laboratory (http://lsro.epfl.ch)
+	EPFL Ecole polytechnique federale de Lausanne (http://www.epfl.ch)
+	
+	See AUTHORS for more details about other contributors.
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <p33fxxxx.h>
+
+#include "flash.h"
+#include "../error/error.h"
+
+
+#define ERASE               0x4042
+#define PROGRAM_ROW         0x4001
+#define PROGRAM_WORD        0x4003
+
+#define INSTRUCTIONS_PER_PAGE  512
+#define INSTRUCTION_PER_ROW   64
+
+#define tblwtl(offset, data) do { \
+			asm __volatile__ ("tblwtl %[d], [%[o]]" : /* No output */ \
+												   : [d] "r" (data) , [o] "r" (offset)); \
+								} while(0)
+								
+#define tblwth(offset, data) do { \
+			asm __volatile__ ("tblwth %[d], [%[o]]" : /* No output */ \
+												   : [d] "r" (data) , [o] "r" (offset)); \
+								} while(0)
+
+static unsigned int row_counter = 0;
+static unsigned long current_addr = 0;
+static unsigned int saved_tblpag;
+
+
+static void  do_key_seq(void) {
+	unsigned int flags;
+	// Disable IRQ
+	SET_AND_SAVE_CPU_IPL(flags, 7);
+	
+	asm __volatile__ (
+	"disi    #5\n"
+    "mov     #0x55,W0\n"
+    "mov     W0, NVMKEY\n"
+    "mov     #0xAA, W0\n"
+    "mov     W0, NVMKEY\n"
+    "bset    NVMCON, #15\n"
+    "nop\n"
+    "nop\n"
+    "btsc    NVMCON, #15\n"
+    "bra     $-2\n"
+     : /* No output */
+     : /* No input */ 
+     : "w0", "cc", "memory");
+     
+     // Reenable IRQ
+     RESTORE_CPU_IPL(flags);
+     
+     return
+	;
+}
+
+
+unsigned long flash_read_instr(unsigned long addr) {
+	unsigned int tmp;
+	unsigned int high, low;
+	tmp = TBLPAG;
+	
+	TBLPAG = addr >> 16;
+	high = __builtin_tblrdh(addr & 0xFFFF);
+	low = __builtin_tblrdl(addr & 0xFFFF);
+	
+	TBLPAG = tmp;
+	
+	return (unsigned long) high << 16 | low;
+}
+
+unsigned int flash_read_low(unsigned long addr) {
+	unsigned int tmp;
+	unsigned int low;
+	tmp = TBLPAG;
+	
+	TBLPAG = addr >> 16;
+	low = __builtin_tblrdl(addr & 0xFFFF);
+	
+	TBLPAG = tmp;
+	
+	return low;
+}
+
+unsigned char flash_read_high(unsigned long addr) {
+	unsigned int tmp;
+	unsigned int high;
+	tmp = TBLPAG;
+	
+	TBLPAG = addr >> 16;
+	high = __builtin_tblrdh(addr & 0xFFFF);
+	
+	TBLPAG = tmp;
+	
+	return high;
+}
+
+static void _frc_nc(unsigned long addr, size_t size, unsigned char * buffer) {
+	unsigned int tmp;
+	unsigned int data;
+	size_t i;
+	size_t instr = size / 3;
+	size_t left = size % 3;
+	
+	tmp = TBLPAG;
+	TBLPAG = addr >> 16;
+	
+	for(i = 0; i < instr; i++, addr+=2) {
+		data = __builtin_tblrdl(addr & 0xFFFF);
+		*buffer++ = data & 0xFF;
+		*buffer++ = data >> 8;
+		data =  __builtin_tblrdh(addr & 0xFFFF);
+		*buffer++ = data & 0xFF;
+	}
+	if(left > 1) {
+		data = __builtin_tblrdl(addr & 0xFFFF);
+		*buffer++ = data & 0xFF;
+		if(left > 2) 
+			*buffer = data >> 8;
+	}
+	TBLPAG = tmp;
+}
+
+void flash_read_chunk(unsigned long addr, size_t size, unsigned char * buffer) {
+	do {
+		unsigned long next_tbl = ((addr >> 16) + 1) << 16;
+		unsigned long bytes_nt = ((next_tbl - addr) / 2 ) * 3;
+		unsigned long min = bytes_nt < size ? bytes_nt : size;
+		_frc_nc(addr, min, buffer);
+		addr += min / 3 * 2;
+		buffer += min;
+		size -= min;
+	} while(size);
+}
+
+void flash_erase_page(unsigned long addr) {
+	unsigned int tmp;
+
+	if(addr & (INSTRUCTIONS_PER_PAGE * 2 - 1)) 
+		ERROR(FLASH_UNALIGNED_ADDRESS, &addr);
+	
+	tmp = TBLPAG;
+	TBLPAG = addr >> 16;
+	
+	NVMCON = ERASE;
+	
+	tblwtl(addr & 0xFFFF, addr & 0xFFFF);
+	
+	do_key_seq();
+	
+	TBLPAG = tmp;
+}
+
+void flash_prepare_write(unsigned long addr) {
+	if(addr & (INSTRUCTIONS_PER_PAGE * 2 - 1)) 
+		ERROR(FLASH_UNALIGNED_ADDRESS, &addr);
+	current_addr = addr;
+	row_counter = 0;
+	
+	saved_tblpag = TBLPAG;
+	TBLPAG = addr >> 16;
+	NVMCON = PROGRAM_ROW;
+}
+
+void flash_write_instruction(unsigned long data) {
+	tblwtl(current_addr & 0xFFFF, data & 0xFFFF);
+	tblwth(current_addr & 0xFFFF, data >> 16);
+	current_addr += 2;
+	row_counter++;
+	if(row_counter == INSTRUCTION_PER_ROW) {
+		do_key_seq(); 
+		TBLPAG = current_addr >> 16; 
+		NVMCON = PROGRAM_ROW;
+		row_counter = 0;
+	}
+}
+
+void flash_write_buffer(unsigned char * data, size_t size) {
+	size_t i;
+	size_t instr = size / 3;
+	size_t left = size % 3;
+	
+	for( i = 0; i < instr; i++, data += 3)
+		flash_write_instruction(data[0] | (unsigned long) data[1] << 8 |  (unsigned long) data[2] << 16);
+	
+	if(left == 1) 
+		flash_write_instruction(data[0]);
+	if(left == 2)
+		flash_write_instruction(data[0] | (unsigned long) data[1]);
+	
+}
+
+
+void flash_complete_write(void) {
+	while(row_counter) 
+		flash_write_instruction(0xFFFFFFFF);
+}
+
---
+++ flash/flash.h
@@ -0,0 +1,78 @@
+
+
+/*
+	Molole - Mobots Low Level library
+	An open source toolkit for robot programming using DsPICs
+	
+	Copyright (C) 2008 Stephane Magnenat <stephane at magnenat dot net>,
+	Philippe Retornaz <philippe dot retornaz at epfl dot ch>
+	Mobots group (http://mobots.epfl.ch), Robotics system laboratory (http://lsro.epfl.ch)
+	EPFL Ecole polytechnique federale de Lausanne (http://www.epfl.ch)
+	
+	See AUTHORS for more details about other contributors.
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef _MOLOLE_FLASH_H
+#define _MOLOLE_FLASH_H
+
+#include "../types/types.h"
+#include <stdlib.h>
+
+/** \addtogroup flash */
+/*@{*/
+
+/** \file
+	Flash access definitions
+*/
+
+/** Errors Flash module can throw */
+enum flash_errors
+{
+	FLASH_ERROR_BASE = 0x0E00,
+	FLASH_UNALIGNED_ADDRESS,
+	FLASH_BAD_SIZE,
+};
+
+// Read the whole 24 bit word
+unsigned long flash_read_instr(unsigned long addr);
+
+// Read the 16 low bits
+unsigned int flash_read_low(unsigned long addr);
+
+// Read the 8 higer bits
+unsigned char flash_read_high(unsigned long addr);
+
+// Read each 24 bits word and put it in a buffer (no "phantom" 8 higher bits)
+void flash_read_chunk(unsigned long addr, size_t size, unsigned char * buffer);
+
+// Erase the page pointed by addr 
+void flash_erase_page(unsigned long addr);
+
+// Prepare a write operation, must be completed with "flash_complete_write();
+// ONLY "WRITE" call are allowed between a prepare and a complete !
+void flash_prepare_write(unsigned long addr);
+
+// Write an instruction at the current writing address.
+void flash_write_instruction(unsigned long data);
+
+// write a buffer filled with 24 bits word data (no "Phantom" 8 higher bits)
+void flash_write_buffer(unsigned char * data, size_t size);
+
+// Finish a write. Flush all buffers etc ... 
+void flash_complete_write(void);
+
+#endif
