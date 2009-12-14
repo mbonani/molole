bba50415f42ec07ef98372a48ef7f14352659b1d
Philippe Rétornaz <philippe.retornaz@epfl.ch>
2009-12-14 08:26:20 +0000

    Compile support only for existing IC

--- ic/ic.c
+++ ic/ic.c
@@ -59,6 +59,8 @@ static struct
 } IC_Data[8];
 
 
+#define IC_EXIST(p) (defined _IC## p ##IF)              // macro to check if an input capture channel exists in this particular dsPIC model
+
 //-------------------
 // Exported functions
 //-------------------
@@ -92,6 +94,7 @@ void ic_enable(int ic_id, int source, int mode, ic_callback callback, int priori
 	
 	switch (ic_id)
 	{
+#if IC_EXIST(1)
 		case IC_1:
 		IC1CONbits.ICM = IC_DISABLED;
 		IC1CONbits.ICTMR = source;
@@ -102,7 +105,8 @@ void ic_enable(int ic_id, int source, int mode, ic_callback callback, int priori
 		_IC1IF = 0;
 		_IC1IE = 1;
 		break;
-		
+#endif
+#if IC_EXIST(2)
 		case IC_2:
 		IC2CONbits.ICM = IC_DISABLED;
 		IC2CONbits.ICTMR = source;
@@ -113,7 +117,8 @@ void ic_enable(int ic_id, int source, int mode, ic_callback callback, int priori
 		_IC2IF = 0;
 		_IC2IE = 1;
 		break;
-		
+#endif
+#if IC_EXIST(3)	
 		case IC_3:
 		IC3CONbits.ICM = IC_DISABLED;
 		IC3CONbits.ICTMR = source;
@@ -124,7 +129,8 @@ void ic_enable(int ic_id, int source, int mode, ic_callback callback, int priori
 		_IC3IF = 0;
 		_IC3IE = 1;
 		break;
-		
+#endif
+#if IC_EXIST(4)
 		case IC_4:
 		IC4CONbits.ICM = IC_DISABLED;
 		IC4CONbits.ICTMR = source;
@@ -135,7 +141,8 @@ void ic_enable(int ic_id, int source, int mode, ic_callback callback, int priori
 		_IC4IF = 0;
 		_IC4IE = 1;
 		break;
-		
+#endif
+#if IC_EXIST(5)
 		case IC_5:
 		IC5CONbits.ICM = IC_DISABLED;
 		IC5CONbits.ICTMR = source;
@@ -146,7 +153,8 @@ void ic_enable(int ic_id, int source, int mode, ic_callback callback, int priori
 		_IC5IF = 0;
 		_IC5IE = 1;
 		break;
-		
+#endif
+#if IC_EXIST(6)
 		case IC_6:
 		IC6CONbits.ICM = IC_DISABLED;
 		IC6CONbits.ICTMR = source;
@@ -157,7 +165,8 @@ void ic_enable(int ic_id, int source, int mode, ic_callback callback, int priori
 		_IC6IF = 0;
 		_IC6IE = 1;
 		break;
-		
+#endif
+#if IC_EXIST(7)
 		case IC_7:
 		IC7CONbits.ICM = IC_DISABLED;
 		IC7CONbits.ICTMR = source;
@@ -168,7 +177,8 @@ void ic_enable(int ic_id, int source, int mode, ic_callback callback, int priori
 		_IC7IF = 0;
 		_IC7IE = 1;
 		break;
-		
+#endif
+#if IC_EXIST(8)	
 		case IC_8:
 		IC8CONbits.ICM = IC_DISABLED;
 		IC8CONbits.ICTMR = source;
@@ -179,6 +189,7 @@ void ic_enable(int ic_id, int source, int mode, ic_callback callback, int priori
 		_IC8IF = 0;
 		_IC8IE = 1;
 		break;
+#endif
 		
 		default: ERROR(IC_ERROR_INVALID_IC_ID, &ic_id);
 	}
@@ -194,14 +205,30 @@ void ic_disable(int ic_id)
 {
 	switch (ic_id)
 	{
+#if IC_EXIST(1)
 		case IC_1: IC1CONbits.ICM = IC_DISABLED; break;
+#endif
+#if IC_EXIST(2)
 		case IC_2: IC2CONbits.ICM = IC_DISABLED; break;
+#endif
+#if IC_EXIST(3)
 		case IC_3: IC3CONbits.ICM = IC_DISABLED; break;
+#endif
+#if IC_EXIST(4)
 		case IC_4: IC4CONbits.ICM = IC_DISABLED; break;
+#endif
+#if IC_EXIST(5)
 		case IC_5: IC5CONbits.ICM = IC_DISABLED; break;
+#endif
+#if IC_EXIST(6)
 		case IC_6: IC6CONbits.ICM = IC_DISABLED; break;
+#endif
+#if IC_EXIST(7)
 		case IC_7: IC7CONbits.ICM = IC_DISABLED; break;
+#endif
+#if IC_EXIST(8)
 		case IC_8: IC8CONbits.ICM = IC_DISABLED; break;
+#endif
 		default: ERROR(IC_ERROR_INVALID_IC_ID, &ic_id);
 	}
 }
@@ -215,6 +242,7 @@ void ic_disable(int ic_id)
  
 	Call the user-defined function.
 */
+#if IC_EXIST(1)
 void _ISR _IC1Interrupt(void)
 {
 	// Clear interrupt flag
@@ -222,12 +250,13 @@ void _ISR _IC1Interrupt(void)
 
 	IC_Data[0].callback(IC_1, IC1BUF, IC_Data[0].user_data);
 }
-
+#endif
 /**
 	Input Capture 2 Interrupt Service Routine.
  
 	Call the user-defined function.
 */
+#if IC_EXIST(2)
 void _ISR _IC2Interrupt(void)
 {
 	// Clear interrupt flag
@@ -235,12 +264,13 @@ void _ISR _IC2Interrupt(void)
 
 	IC_Data[1].callback(IC_2, IC2BUF, IC_Data[1].user_data);
 }
-
+#endif
 /**
 	Input Capture 3 Interrupt Service Routine.
  
 	Call the user-defined function.
 */
+#if IC_EXIST(3)
 void _ISR _IC3Interrupt(void)
 {
 	// Clear interrupt flag
@@ -248,12 +278,13 @@ void _ISR _IC3Interrupt(void)
 
 	IC_Data[2].callback(IC_3, IC3BUF, IC_Data[2].user_data);
 }
-
+#endif
 /**
 	Input Capture 4 Interrupt Service Routine.
  
 	Call the user-defined function.
 */
+#if IC_EXIST(4)
 void _ISR _IC4Interrupt(void)
 {
 	// Clear interrupt flag
@@ -261,12 +292,13 @@ void _ISR _IC4Interrupt(void)
 
 	IC_Data[3].callback(IC_4, IC4BUF, IC_Data[3].user_data);
 }
-
+#endif
 /**
 	Input Capture 5 Interrupt Service Routine.
  
 	Call the user-defined function.
 */
+#if IC_EXIST(5)
 void _ISR _IC5Interrupt(void)
 {
 	// Clear interrupt flag
@@ -274,12 +306,13 @@ void _ISR _IC5Interrupt(void)
 
 	IC_Data[4].callback(IC_5, IC5BUF, IC_Data[4].user_data);
 }
-
+#endif
 /**
 	Input Capture 6 Interrupt Service Routine.
  
 	Call the user-defined function.
 */
+#if IC_EXIST(6)
 void _ISR _IC6Interrupt(void)
 {
 	// Clear interrupt flag
@@ -287,12 +320,13 @@ void _ISR _IC6Interrupt(void)
 
 	IC_Data[5].callback(IC_6, IC6BUF, IC_Data[5].user_data);
 }
-
+#endif
 /**
 	Input Capture 7 Interrupt Service Routine.
  
 	Call the user-defined function.
 */
+#if IC_EXIST(7)
 void _ISR _IC7Interrupt(void)
 {
 	// Clear interrupt flag
@@ -300,12 +334,13 @@ void _ISR _IC7Interrupt(void)
 
 	IC_Data[6].callback(IC_7, IC7BUF, IC_Data[6].user_data);
 }
-
+#endif
 /**
 	Input Capture 8 Interrupt Service Routine.
  
 	Call the user-defined function.
 */
+#if IC_EXIST(8)
 void _ISR _IC8Interrupt(void)
 {
 	// Clear interrupt flag
@@ -313,5 +348,5 @@ void _ISR _IC8Interrupt(void)
 
 	IC_Data[7].callback(IC_8, IC8BUF, IC_Data[7].user_data);
 }
-
+#endif
 /*@}*/
