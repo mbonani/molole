221ac971c6eefff8259a0cd3bff72ab169154d0e
Stéphane Magnenat <stephanemagnenat@users.noreply.github.com>
2011-05-09 10:11:00 +0000

    Relicensed Molole under LGPL.

--- types/types.h
+++ types/types.h
@@ -1,25 +1,25 @@
 /*
 	Molole - Mobots Low Level library
 	An open source toolkit for robot programming using DsPICs
-	
-	Copyright (C) 2007 - 2008 Stephane Magnenat <stephane at magnenat dot net>,
+
+	Copyright (C) 2007--2011 Stephane Magnenat <stephane at magnenat dot net>,
+	Philippe Retornaz <philippe dot retornaz at epfl dot ch>
 	Mobots group (http://mobots.epfl.ch), Robotics system laboratory (http://lsro.epfl.ch)
 	EPFL Ecole polytechnique federale de Lausanne (http://www.epfl.ch)
-	
-	See AUTHORS for more details about other contributors.
-	
+
+	See authors.txt for more details about other contributors.
+
 	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-	
+	it under the terms of the GNU Lesser General Public License as published
+	by the Free Software Foundation, version 3 of the License.
+
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-	
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef _MOLOLE_TYPES_H
